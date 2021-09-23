8011b793774000ca8c6b4b21c0b746d751891001
Wraient Daemon <81632222+Wraient@users.noreply.github.com>
2021-09-23 18:22:09 +0530

    Add files via upload

---
+++ 006.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char a;
+    cout << "Enter a alphabetic CHARACTER : ";
+    cin >> a;
+    switch (a)
+    {
+    case 'a':
+        cout << "hello";
+        break;
+    case 'b':
+        cout << "Hola";
+        break;
+    case 'c':
+        cout << "Hie";
+        break;
+    case 'd':
+        cout << "Lol you ain't human";
+        break;
+    case 'e':
+        cout << "Get a life bro";
+        break;
+    case 'f':
+        cout << "why am i here bro i dont even know how to code";
+        break;
+    case 'g':
+        cout << "Send help";
+        break;
+    case 'h':
+        cout << "Go FUCK ur self FREAK(i dont even know what that means)";
+        break;
+    default:
+        cout << "you are depressed piece of shit";
+        break;
+    }
+
+    return 0;
+}
---
+++ 007.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int i=10, j=20,k;
+    // //   11   20/21 11/12 22   12/13  23    23/24;
+    // k = ++i + j++ + i++ + ++j + i++ + ++j + j++;
+    // cout << k;
+
+    // == != > < >= <=
+
+    // cout << "Entre a number : ";
+    // int num;
+    // cin >> num;
+    // if(num<=10){cout << "YEEEEEEEAAAAAAAAAAAHHHHHHHHHHHHHHH";}
+
+    // &&=and ||=or !=not
+
+    // cout << "Entre a number : ";
+    // int num;
+    // cin >> num;
+    // if((num%2)==0 && (num%3)==0){cout << "Both 2 and 3 are divisible by " << num << endl;}
+    // else if((num%2)==0){cout << "2 is divisible by " << num << " but not 3" << endl;}
+    // else if((num%3)==0){cout << "3 is divisible by " << num << " but not 2" << endl;}
+    // else{cout << "None of 2 and 3 are divisible by " << num << endl;}
+
+    
+
+
+
+    return 0;
+}
