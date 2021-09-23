9e4c65d477d1310b2e5093ac9a609091035611fb
Wraient Daemon <81632222+Wraient@users.noreply.github.com>
2021-09-23 18:39:43 +0530

    Update 007.cpp

--- 007.cpp
+++ 007.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     // int i=10, j=20,k;
-    // //   11   20/21 11/12 22   12/13  23    23/24;
+    //   11   20/21 11/12 22   12/13  23    23/24;
     // k = ++i + j++ + i++ + ++j + i++ + ++j + j++;
     // cout << k;
 
@@ -25,9 +25,25 @@ int main()
     // else if((num%3)==0){cout << "3 is divisible by " << num << " but not 2" << endl;}
     // else{cout << "None of 2 and 3 are divisible by " << num << endl;}
 
-    
+    // cout << "Entre a number : ";
+    // int num;
+    // cin >> num;
+    // (num<10)? cout << "num<10" : (num==10)? cout<<"num==10" : cout << "num>10";
 
+    //cast char a ='a'; cout << int(a);
 
+    // int a = (2,3,4), a=4;
+
+    // int a = 3;
+    // cout << &(a); // address of the variable a
+
+    // int a = 3;
+    // int c = 3;
+    // cout << &(a);
+    // cout << endl;
+    // cout << b;
+    // cout << endl;
+    // cout << &(c);
 
     return 0;
 }
